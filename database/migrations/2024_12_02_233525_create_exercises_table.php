5064dd847a8bd6550173c1f0a4f844b652d25a30
lucasoliv21 <lucascodantas@hotmail.com>
2024-12-02 20:42:19 -0300

    creating migration for exercises

--- database/migrations/2024_12_02_233525_create_exercises_table.php
+++ database/migrations/2024_12_02_233525_create_exercises_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('exercises', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('muscle_group');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
