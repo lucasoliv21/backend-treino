48e8539f34a5bb1cc38113f1761417daeb8e1e1a
lucasoliv21 <lucascodantas@hotmail.com>
2024-12-02 20:55:59 -0300

    set relationship from WorkoutSession

--- app/Models/WorkoutSession.php
+++ app/Models/WorkoutSession.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class WorkoutSession extends Model
 {
     //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function workoutPlan()
+    {
+        return $this->belongsTo(WorkoutPlan::class);
+    }
+
+    public function workoutDetails()
+    {
+        return $this->hasMany(WorkoutDetail::class);
+    }
+
 }
